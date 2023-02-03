0cc86ece131793f6c8feb57b2f525fdecdd3e216
Robert Buentgen <rBuentgen@gmail.com>
2023-02-02 17:39:46 -0700

    Edited script to have LEDs fire off in order with a timer

--- Ardity/Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
+++ Ardity/Assets/Ardity/Scripts/Samples/SampleUserPolling_ReadWrite.cs
@@ -15,76 +15,49 @@ using System.Collections;
 public class SampleUserPolling_ReadWrite : MonoBehaviour
 {
     public SerialController serialController;
+    enum Scents { Pine, Citrus, Peanut_Butter, Lavender };
 
+    [SerializeField] Scents ScentOne;
+    [SerializeField] Scents ScentTwo;
+    [SerializeField] Scents ScentThree;
+    [SerializeField] Scents ScentFour;
+
+    [SerializeField] Scents AtomizerOneContents;
+    [SerializeField] Scents AtomizerTwoContents;
+    [SerializeField] Scents AtomizerThreeContnets;
+    [SerializeField] Scents AtomizerFourContents;
+
+    //Timer
+    public float scentTime = 60.0f;
+    public float breakTime = 5.0f;
+
+    [SerializeField]  private int currentScent = 1;
+    private bool firstAtomizerOn = false;
+    private bool secondAtomizerOn = false;
+    private bool thirdAtomizerOn = false;
+    private bool fourthAtomizerOn = false;
+
+    [SerializeField] private float scentTimer;
+    [SerializeField] private float breakTimer;
     // Initialization
     void Start()
     {
         serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
-
-        Debug.Log("Press A or Z to execute some actions");
+        scentTimer = scentTime;
+        breakTimer = breakTime;
     }
 
     // Executed each frame
     void Update()
     {
-        //---------------------------------------------------------------------
-        // Send data
-        //---------------------------------------------------------------------
-
-        // If you press one of these keys send it to the serial device. A
-        // sample serial device that accepts this input is given in the README.
-
-        //RED led On - Off Start
-        if (Input.GetKeyDown(KeyCode.Q))
+        RunTest();
+        if(currentScent < 5) // Becase there are only 4 scents and we dont need the timer to keep running afer that
         {
-            Debug.Log("Sending Q -- RED LED ON");
-            serialController.SendSerialMessage("Q");
+        scentTimer -= Time.deltaTime;
+        if(scentTimer < 0) breakTimer -= Time.deltaTime; //run break timer when scent timer is up
         }
 
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log("Sending A -- RED LED OFF");
-            serialController.SendSerialMessage("A");
-        }
-        //RED led On - Off End
-
-        //YELLOW Led on - off Start
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Debug.Log("Sending W -- YELLOW LED ON");
-            serialController.SendSerialMessage("W");
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Debug.Log("Sending S -- YELLOW LED OFF");
-            serialController.SendSerialMessage("S");
-        }
-        //Yellow Led on - off End
-        //Blue Led on - off Start
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            Debug.Log("Sending E -- BLUE LED ON");
-            serialController.SendSerialMessage("E");
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            Debug.Log("Sending D -- BLUE LED OFF");
-            serialController.SendSerialMessage("D");
-        }
-        //Blue Led on - off End
-        //Green Led on - off Start
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Debug.Log("Sending R -- GREEN LED ON");
-            serialController.SendSerialMessage("R");
-        }
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            Debug.Log("Sending F -- GREEN LED OFF");
-            serialController.SendSerialMessage("F");
-        }
-        //Green Led on - off End
+        
 
         //---------------------------------------------------------------------
         // Receive data
@@ -102,5 +75,100 @@ public class SampleUserPolling_ReadWrite : MonoBehaviour
             Debug.Log("Connection attempt failed or disconnection detected");
         else
             Debug.Log("Message arrived: " + message);
+    }
+
+    private void RunTest()
+    {
+        if (currentScent == 1)
+        {
+            if (scentTimer > 0)ToggleFirstAtomizer(true);
+            else
+            {
+                ToggleFirstAtomizer(false);
+                if (breakTimer < 0) ResetTimes();  //Wait for break to be up before continuing onto next scent
+            }
+        }else if(currentScent == 2)
+        {
+            if (scentTimer > 0) ToggleSecondAtomizer(true);
+            else
+            {
+                ToggleSecondAtomizer(false);
+                if (breakTimer < 0) ResetTimes();   //Wait for break to be up before continuing onto next scent
+            }
+        }else if (currentScent == 3)
+        {
+            if (scentTimer > 0) ToggleThirdAtomizer(true);
+            else
+            {
+                ToggleThirdAtomizer(false);
+                if (breakTimer < 0) ResetTimes();   //Wait for break to be up before continuing onto next scent
+            }
+        }else if (currentScent == 4)
+        {
+            if (scentTimer > 0) ToggleFourthAtomizer(true);
+            else
+            {
+                ToggleFourthAtomizer(false);
+                if (breakTimer < 0) ResetTimes();   //Wait for break to be up before continuing onto next scent
+            }
+        }
+
+
+
+    }
+
+    private void ToggleFirstAtomizer(bool status)
+    {
+        if (status == firstAtomizerOn) return;
+        firstAtomizerOn = status;
+
+        if(status == true) serialController.SendSerialMessage("Q"); //Turn on the first atomizer
+        if(status == false) serialController.SendSerialMessage("A"); //Turn off the first atomizer
+
+        if (status == true) Debug.Log("First Atomizer On");
+        else Debug.Log("First Atomizer off");
+    }
+
+    private void ToggleSecondAtomizer(bool status)
+    {
+        if (status == secondAtomizerOn) return;
+        secondAtomizerOn = status;
+
+        if (status == true) serialController.SendSerialMessage("W"); //Turn on the Second atomizer
+        if (status == false) serialController.SendSerialMessage("S"); //Turn off the Second atomizer
+
+        if (status == true) Debug.Log("Second Atomizer On");
+        else Debug.Log("Second Atomizer off");
+    }
+
+    private void ToggleThirdAtomizer(bool status)
+    {
+        if (status == thirdAtomizerOn) return;
+        thirdAtomizerOn = status;
+
+        if (status == true) serialController.SendSerialMessage("E"); //Turn on the Third atomizer
+        if (status == false) serialController.SendSerialMessage("D"); //Turn off the Third atomizer
+
+        if (status == true) Debug.Log("Third Atomizer On");
+        else Debug.Log("Third Atomizer off");
+    }
+
+    private void ToggleFourthAtomizer(bool status)
+    {
+        if (status == fourthAtomizerOn) return;
+        fourthAtomizerOn = status;
+
+        if (status == true) serialController.SendSerialMessage("R"); //Turn on the Fourth atomizer
+        if (status == false) serialController.SendSerialMessage("F"); //Turn off the Fourth atomizer
+
+        if (status == true) Debug.Log("Fourth Atomizer On");
+        else Debug.Log("Fourth Atomizer off");
+    }
+
+    private void ResetTimes()
+    {
+        scentTimer = scentTime;
+        breakTimer = breakTime;
+        currentScent += 1;
     }
 }
